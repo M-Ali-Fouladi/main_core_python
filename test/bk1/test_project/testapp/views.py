afaf9b616a8e221b8b98949c547cbac39fca12e5
Mohammadali <Mohammadali@DESKTOP-599L92V>
2023-11-05 16:33:35 +0330

    adding main_core

---
+++ test/bk1/test_project/testapp/views.py
@@ -0,0 +1,42 @@
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import render, redirect
+from .forms import LoginForm, RegistrationForm
+
+def home_view(request):
+    username = request.user.username if request.user.is_authenticated else None
+    return render(request, 'home.html', {'username': username})
+
+
+def login_view(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+            else:
+                form.add_error(None, 'Invalid username or password.')
+    else:
+        form = LoginForm()
+
+    return render(request, 'login.html', {'form': form})
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
+
+
+def register_view(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = RegistrationForm()
+
+    return render(request, 'register.html', {'form': form})
---
+++ test/test_project/testapp/models.py
@@ -0,0 +1,31 @@
+
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+ 
+class CustomUser(AbstractUser):
+
+   ROLE_CHOICES={
+        ('admin','admin'),
+        ('user', 'user'),
+        ('guest', 'guest'),
+    }
+   role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='user')
+
+
+
+"""
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=15)
+"""
+
+    # Add your custom fields here, for example:
+   
+
+    # Add more custom fields as needed
+
+def __str__(self):
+    return self.user.username
+
